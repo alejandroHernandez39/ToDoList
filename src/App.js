5ee95bfa4ca12a22cc2f3d6a8ffd895cd10e729e
Alejandro <ale101rosales@gmail.com>
2025-04-27 18:37:22 -0600

    Actividad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Menu from './Components/Menu/Menu';
+import Form from './Components/Form/Form';
+import Item from './Components/Item/Item';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu></Menu>
+      <Container>
+        <Row>
+        <Col><Form></Form></Col>
+        <Col>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        </Col>
+        </Row>
+      </Container> 
     </div>
   );
 }
